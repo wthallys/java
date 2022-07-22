8d4553ef277ccbe92f9735ba1354c9a7a2ad3866
wthallys <wthallys.02@gmail.com>
2022-07-22 01:51:23 -0300

    practing java

---
+++ problemaProduto/src/entities/Product.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock() {
+        return quantity * price;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    //sobrepondo o método toString para um modo formatado
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
