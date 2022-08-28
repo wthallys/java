bccf2290c43df443b958abc7574bd467e4b9dba8
wthallys <wthallys.02@gmail.com>
2022-08-28 04:53:19 -0300

    Added .gitignore and new exercise

---
+++ SolvedExercise/src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+        System.out.println("Enter worker data: ");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        System.out.print("Base salary: ");
+        double baseSalary = sc.nextDouble();
+
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+
+        System.out.print("How many contracts to this worker? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter a contract #" + i + " data: ");
+            System.out.print("Date (DD/MM/YYYY): ");
+//            String stringContractDate = sc.nextLine();
+            LocalDate contractDate = LocalDate.parse(sc.nextLine(), fmt1);
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            System.out.print("Duration (hours): ");
+            int hours = sc.nextInt();
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+
+        System.out.println();
+        System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+
+    }
+}
