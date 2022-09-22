582d28add9ff98cc442210728ab2d373b453387a
wthallys <wthallys.02@gmail.com>
2022-09-22 02:58:12 -0300

    solvedExercise added

---
+++ Seção 14 - Herança e Polimorfismo/solvedExercise/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.println("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Employee #" + i + " data: ");
+            System.out.print("Outsourced (y/n)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            double hours = sc.nextDouble();
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextInt();
+            if (ch == 'y') {
+                System.out.println();
+            }
+        }
+
+    }
+}
