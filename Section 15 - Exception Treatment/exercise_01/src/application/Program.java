d9d8cce082465399a225b0ba128224b613cad612
wthallys <wthallys.02@gmail.com>
2023-01-10 09:31:57 -0300

    exercise 01 added

---
+++ Section 15 - Exception Treatment/exercise_01/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (dd/MM/yyyyy): ");
+        LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Check-out date (dd/MM/yyyyy): ");
+        LocalDate checkOut = LocalDate.parse(sc.next(), fmt);
+
+        if (!checkOut.isAfter(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+        }
+    }
+}
---
+++ Section 15 - Exception Treatment/exercise_01/src/model/entities/Reservation.java
@@ -0,0 +1,61 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+import static java.time.temporal.ChronoUnit.MILLIS;
+
+public class Reservation {
+
+    private int roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation(int roomNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        long diff = MILLIS.between(checkOut, checkIn);
+        return TimeUnit.MILLISECONDS.toDays(diff);
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + fmt.format(checkIn)
+                + ", check-out: "
+                + fmt.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
